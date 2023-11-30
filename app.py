1bbb75d3cc5af78940baccdfbd3a06aa03167976
kashfun <60571031+kashfun@users.noreply.github.com>
2023-11-30 22:55:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,32 +3,28 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import hashlib  # Import hashlib for password hashing
 
-# Create a Flask web application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
-# Initialize SQLAlchemy for database operations
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 
-# Define the User model for database operations
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
 
-# Configure the user loader for Flask-Login
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-# Routes
 @app.route('/')
 def home():
     return 'Home Page'
@@ -39,7 +35,12 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        user = User.query.filter_by(username=username, password=password).first()
+
+        # Simulate a vulnerability by using an unsafe password storage method
+        # Replace this with a secure password hashing mechanism in a production scenario
+        hashed_password = hashlib.md5(password.encode()).hexdigest()
+
+        user = User.query.filter_by(username=username, password=hashed_password).first()
         if user:
             login_user(user)
             flash('Login successful!', 'success')
@@ -56,8 +57,6 @@ def logout():
     return redirect(url_for('home'))
 
 
-# Server setup
 if __name__ == '__main__':
-    # Create database tables before running the application
     db.create_all()
     app.run(debug=True)
